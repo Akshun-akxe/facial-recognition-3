a0f34f62585da36e28d7074e7e957bee4fb551d8
Akshun-akxe <71005076+Akshun-akxe@users.noreply.github.com>
2021-07-14 00:24:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+Webcam.set({
+    width: 350,
+    height: 350,
+    image_format: 'png',
+    png_quality: 9999
+});
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="Captured_Image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/He-zs46qk/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded');
+}
+
+function check() {
+    img = document.getElementById('Captured_Image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_person_name").innerHTML = results[0].label;
+        document.getElementById("result_person_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
